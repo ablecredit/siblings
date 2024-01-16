f647336d0dc6beb31fcc5665cc3710fe6587cdd1
Anubhab Bandyopadhyay <anubhab.bandyopadhyay@gmail.com>
2024-01-16 19:23:06 +0530

    Region part of request not initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(let_chains)]
 
-use std::{sync::Arc, collections::HashMap};
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Result;
 use db::Db;
@@ -14,22 +14,21 @@ extern crate log;
 pub struct Siblings<'a> {
     db: Arc<Db>,
     me: Option<&'a str>, // define who is me - this has to be the template code
-    region: Regions,
-    endpoints: Arc<RwLock<Endpoints>>
+    endpoints: Arc<RwLock<Endpoints>>,
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Regions {
     IN,
-    US
+    US,
 }
 
 impl From<&str> for Regions {
     fn from(value: &str) -> Self {
         match value {
-            "IN"|"IND" => Self::IN,
-            "US"|"USA" => Self::US,
-            _ => panic!("Region {value} not supported")
+            "IN" | "IND" => Self::IN,
+            "US" | "USA" => Self::US,
+            _ => panic!("Region {value} not supported"),
         }
     }
 }
@@ -43,24 +42,30 @@ pub struct Endpoints {
     sentry: Option<RegionEndpoint>,
     siblings: HashMap<String, RegionEndpoint>,
     thumbnailer: Option<RegionEndpoint>,
-    xchange: Option<RegionEndpoint>
+    xchange: Option<RegionEndpoint>,
 }
 
 #[derive(Debug, Clone, Deserialize, Default)]
 pub struct RegionEndpoint {
     default: String,
     ind: Option<String>,
-    usa: Option<String>
+    usa: Option<String>,
 }
 
 impl RegionEndpoint {
-    pub fn get(&self, region: Regions) -> Option<String> {
-        match region {
-            Regions::US => if let Some(us) = &self.usa {
-                return Some(us.clone())
-            }
-            Regions::IN => if let Some(ind) = &self.ind {
-                return Some(ind.clone())
+    pub fn get(&self, region: Option<Regions>) -> Option<String> {
+        if let Some(region) = region {
+            match region {
+                Regions::US => {
+                    if let Some(us) = &self.usa {
+                        return Some(us.clone());
+                    }
+                }
+                Regions::IN => {
+                    if let Some(ind) = &self.ind {
+                        return Some(ind.clone());
+                    }
+                }
             }
         }
 
@@ -68,147 +73,170 @@ impl RegionEndpoint {
     }
 }
 
-impl <'a>Siblings<'a> {
-    pub fn new(db: Arc<Db>, region: &str, me: Option<&'a str>) -> Self {
+impl<'a> Siblings<'a> {
+    pub fn new(db: Arc<Db>, me: Option<&'a str>) -> Self {
         Self {
             me,
             db,
-            region: region.into(),
-            endpoints: Arc::new(RwLock::new(Endpoints::default()))
+            endpoints: Arc::new(RwLock::new(Endpoints::default())),
         }
     }
 
-    pub async fn august(&self) -> Option<String> {
+    pub async fn august(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(august) = &self.endpoints.read().await.august {
-            return august.get(self.region)
+            return august.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-august").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-august").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.august = Some(ep.clone());
 
-            return ep.get(self.region)
+            return ep.get(region);
         }
-        
+
         warn!("august: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn matrix(&self) -> Option<String> {
+    pub async fn matrix(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(matrix) = &self.endpoints.read().await.matrix {
-            return matrix.get(self.region)
+            return matrix.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-matrix").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-matrix").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.matrix = Some(ep.clone());
 
-            return ep.get(self.region)
+            return ep.get(region);
         }
 
         warn!("matrix: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn pandora(&self) -> Option<String> {
+    pub async fn pandora(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(pandora) = &self.endpoints.read().await.pandora {
-            return pandora.get(self.region)
+            return pandora.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-pandora").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-pandora").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.pandora = Some(ep.clone());
 
-            return ep.get(self.region)
+            return ep.get(region);
         }
 
         warn!("pandora: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn schematron(&self) -> Option<String> {
+    pub async fn schematron(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(schematron) = &self.endpoints.read().await.schematron {
-            return schematron.get(self.region)
+            return schematron.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-schematron").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-schematron").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.pandora = Some(ep.clone());
 
-            return ep.get(self.region)
+            return ep.get(region);
         }
 
         warn!("schematron: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn sentry(&self) -> Option<String> {
+    pub async fn sentry(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(sentry) = &self.endpoints.read().await.sentry {
-            return sentry.get(self.region)
+            return sentry.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-sentry").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-sentry").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.sentry = Some(ep.clone());
 
-            return ep.get(self.region);
+            return ep.get(region);
         }
 
         warn!("sentry: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn thumbnailer(&self) -> Option<String> {
+    pub async fn thumbnailer(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(thumb) = &self.endpoints.read().await.thumbnailer {
-            return thumb.get(self.region)
+            return thumb.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-thumbnailer").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-thumbnailer").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.thumbnailer = Some(ep.clone());
 
-            return ep.get(self.region);
+            return ep.get(region);
         }
 
         warn!("thumbnailer: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn xchange(&self) -> Option<String> {
+    pub async fn xchange(&self, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(x) = &self.endpoints.read().await.xchange {
-            return x.get(self.region)
+            return x.get(region);
         }
-        
-        if let Ok(c) = self.db.get_cache("ep-xchange").await && let Ok(ep) = Self::deserialize(c) {
+
+        if let Ok(c) = self.db.get_cache("ep-xchange").await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.xchange = Some(ep.clone());
 
-            return ep.get(self.region);
+            return ep.get(region);
         }
 
         warn!("xchange: endpoint not found and was not fetched!");
         None
     }
 
-    pub async fn siblings(&self, sibling: &str) -> Option<String> {
+    pub async fn siblings(&self, sibling: &str, region: Option<&str>) -> Option<String> {
+        let region = region.map(|r| r.into());
         if let Some(siblingmap) = self.endpoints.read().await.siblings.get(sibling) {
-            return siblingmap.get(self.region)
+            return siblingmap.get(region);
         }
 
-        if let Ok(c) = self.db.get_cache(format!("ep-{sibling}").as_str()).await && let Ok(ep) = Self::deserialize(c) {
+        if let Ok(c) = self.db.get_cache(format!("ep-{sibling}").as_str()).await
+            && let Ok(ep) = Self::deserialize(c)
+        {
             let mut w = self.endpoints.write().await;
             w.siblings.insert(sibling.to_owned(), ep.clone());
 
-            return ep.get(self.region);
+            return ep.get(region);
         }
 
         warn!("siblings: endpoint for sibling[{sibling}] not found and was not fetched!");
         None
     }
 
-    pub async fn me(&self) -> Option<String> {
+    pub async fn me(&self, region: Option<&str>) -> Option<String> {
         if let Some(me) = self.me {
-            self.siblings(me).await
+            self.siblings(me, region).await
         } else {
             None
         }
@@ -217,22 +245,19 @@ impl <'a>Siblings<'a> {
     fn deserialize(data: Vec<u8>) -> Result<RegionEndpoint> {
         let ep: HashMap<String, String> = serde_json::from_slice(&data[..])?;
 
-        Ok(
-            RegionEndpoint {
-                default: ep.get("default").unwrap().to_string(),
-                ind: ep.get("in").map(|i| i.to_string()),
-                usa: ep.get("us").map(|u| u.to_string()),
-            }
-        )
+        Ok(RegionEndpoint {
+            default: ep.get("default").unwrap().to_string(),
+            ind: ep.get("in").map(|i| i.to_string()),
+            usa: ep.get("us").map(|u| u.to_string()),
+        })
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     // use crate::Siblings;
 
-    use std::{env, fs::read_to_string, collections::HashMap};
+    use std::{collections::HashMap, env, fs::read_to_string};
 
     use crate::Siblings;
 
@@ -241,11 +266,14 @@ mod tests {
         pretty_env_logger::init();
 
         let db = crate::Db::new(env::var("X_PROJECT")?.as_str()).await?;
-        let data = serde_json::from_str::<HashMap<String, HashMap<String, String>>>(read_to_string("siblings.json")?.as_str())?;
+        let data = serde_json::from_str::<HashMap<String, HashMap<String, String>>>(
+            read_to_string("siblings.json")?.as_str(),
+        )?;
 
         for (k, v) in data.iter() {
             let b = serde_json::to_vec(v)?;
-            db.set_cache(format!("ep-{k}").as_str(), &b[..], None).await?;
+            db.set_cache(format!("ep-{k}").as_str(), &b[..], None)
+                .await?;
         }
         Ok(())
     }
@@ -253,33 +281,74 @@ mod tests {
     #[tokio::test]
     async fn check() -> anyhow::Result<()> {
         let db = std::sync::Arc::new(crate::Db::new(env::var("X_PROJECT")?.as_str()).await?);
-        let sib = Siblings::new(db, "IN", None);
+        let sib = Siblings::new(db, None);
 
-        let data = serde_json::from_str::<HashMap<String, HashMap<String, String>>>(read_to_string("siblings.json")?.as_str())?;
+        let data = serde_json::from_str::<HashMap<String, HashMap<String, String>>>(
+            read_to_string("siblings.json")?.as_str(),
+        )?;
 
-        assert_eq!(sib.august().await.as_ref(), data.get("august").unwrap().get("in"));
+        assert_eq!(
+            sib.august(Some("IN")).await.as_ref(),
+            data.get("august").unwrap().get("in")
+        );
 
-        assert_eq!(sib.siblings("bank-statement").await.as_ref(), data.get("bank-statement").unwrap().get("in"));
-        assert_eq!(sib.siblings("bankstat").await.as_ref(), data.get("bankstat").unwrap().get("in"));
+        assert_eq!(
+            sib.siblings("bank-statement", Some("IN")).await.as_ref(),
+            data.get("bank-statement").unwrap().get("in")
+        );
+        assert_eq!(
+            sib.siblings("bankstat", Some("IN")).await.as_ref(),
+            data.get("bankstat").unwrap().get("in")
+        );
 
-        assert_eq!(sib.siblings("credit").await.as_ref(), data.get("credit").unwrap().get("in"));
+        assert_eq!(
+            sib.siblings("credit", Some("IN")).await.as_ref(),
+            data.get("credit").unwrap().get("in")
+        );
 
-        assert_eq!(sib.siblings("finance-statement").await.as_ref(), data.get("finance-statement").unwrap().get("in"));
-        assert_eq!(sib.siblings("finsta").await.as_ref(), data.get("finsta").unwrap().get("in"));
+        assert_eq!(
+            sib.siblings("finance-statement", Some("IN")).await.as_ref(),
+            data.get("finance-statement").unwrap().get("in")
+        );
+        assert_eq!(
+            sib.siblings("finsta", Some("IN")).await.as_ref(),
+            data.get("finsta").unwrap().get("in")
+        );
 
-        assert_eq!(sib.siblings("gstr").await.as_ref(), data.get("gstr").unwrap().get("in"));
+        assert_eq!(
+            sib.siblings("gstr", Some("IN")).await.as_ref(),
+            data.get("gstr").unwrap().get("in")
+        );
 
-        assert_eq!(sib.matrix().await.as_ref(), data.get("matrix").unwrap().get("default"));
+        assert_eq!(
+            sib.matrix(Some("IN")).await.as_ref(),
+            data.get("matrix").unwrap().get("default")
+        );
 
-        assert_eq!(sib.pandora().await.as_ref(), data.get("pandora").unwrap().get("default"));
+        assert_eq!(
+            sib.pandora(Some("IN")).await.as_ref(),
+            data.get("pandora").unwrap().get("default")
+        );
 
-        assert_eq!(sib.schematron().await.as_ref(), data.get("schematron").unwrap().get("default"));
+        assert_eq!(
+            sib.schematron(Some("IN")).await.as_ref(),
+            data.get("schematron").unwrap().get("default")
+        );
 
-        assert_eq!(sib.sentry().await.as_ref(), data.get("sentry").unwrap().get("default"));
+        assert_eq!(
+            sib.sentry(Some("IN")).await.as_ref(),
+            data.get("sentry").unwrap().get("default")
+        );
 
-        assert_eq!(sib.thumbnailer().await.as_ref(), data.get("thumbnailer").unwrap().get("in"));
+        assert_eq!(
+            sib.thumbnailer(Some("IN")).await.as_ref(),
+            data.get("thumbnailer").unwrap().get("in")
+        );
 
-        assert_eq!(sib.xchange().await.as_ref(), data.get("xchange").unwrap().get("default"));
+        assert_eq!(
+            sib.xchange(Some("IN")).await.as_ref(),
+            data.get("xchange").unwrap().get("default")
+        );
 
         Ok(())
     }
